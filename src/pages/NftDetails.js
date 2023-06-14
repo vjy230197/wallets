4215425359aede8ce94740d7339d7fd9480ee045
Vijay <vijay.soam@seracle.com>
2023-06-14 16:10:55 +0530

    1) connect logic

--- src/pages/NftDetails.js
+++ src/pages/NftDetails.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import classes from './NftDetails.module.css'
 import Card from '../components/UI/Card'
 import Loader from '../components/UI/Loader'
 import { Button } from 'react-bootstrap';
+import { ethers } from "ethers";
 
 const NftDetails = (props) => {
 
     const nft_id = useParams().nftid;
     const [nft, setNft] = useState()
+    const [accounts, setAccount] = useState([]);
+    const [balance, setBalance] = useState()
 
     const fetchPost = async () => {
 
@@ -37,6 +40,35 @@ const NftDetails = (props) => {
             fetchPost()
     }, [nft_id])
 
+
+    const connectMetamask = async () => {
+        if (window.ethereum) {
+
+            window.ethereum.request({
+                method: 'eth_requestAccounts'
+            }).then((result) => {
+                accountChangedHandler(result[0])
+
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+        else {
+            console.log('no metamask installed.');
+        }
+    }
+
+    const accountChangedHandler = async (account) => {
+        setAccount(account)
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const balance = await provider.getBalance(account);
+        const nativeBalance = ethers.utils.formatEther(balance);
+
+        await setBalance(nativeBalance)
+    }
+
     return (
         <>
             {
@@ -51,23 +83,39 @@ const NftDetails = (props) => {
                         </div>
                     </div>
                     <div className={classes.right}>
-                        <div className='text-left mb-3'>
-                            <h2>{nft.name}</h2>
+                        <div className={`${classes.price} + text-left mb-3`}>
+                            {nft.name}
                         </div>
                         <div className='text-left mb-5'>
                             Owned by <a href={`https://mumbai.polygonscan.com/address/${nft.owner}`}>{nft.owner.substring(0, 15) + '...'}</a>
                         </div>
                         <div className='mb-5'>
                             <Card maxWidth='40rem' margin='unset'>
-                                <div className='px-4 py-4'>
-                                    <div className='text-left mb-3'>
-                                        <h5>Buy it at</h5>
+                                <div className='px-4 py-4 flex justify-between'>
+                                    <div style={{ width: '100%' }}>
+                                        <div className='text-left mb-3'>
+                                            <h5>Buy it at</h5>
+                                        </div>
+                                        <div className={`${classes.price} + text-left mb-3`}>
+                                            {nft.price} MATIC
+                                        </div>
+                                        <div className='text-left'>
+                                            <Button className={classes.btn}>Buy</Button>
+                                        </div>
                                     </div>
-                                    <div className={`${classes.price} + text-left mb-3`}>
-                                        {nft.price} MATIC
+                                    <div className={classes.line}>
                                     </div>
-                                    <div className='text-left'>
-                                        <Button className={classes.btn}>Buy</Button>
+                                    <div style={{ width: '100%', margin: 'auto' }}>
+                                        <div className='mb-4' onClick={connectMetamask}>
+                                            {accounts.length === 0 && (<span style={{ 'cursor': 'pointer' }} onClick={connectMetamask}>Connect</span>)}
+                                            {accounts.length > 0 && (<span style={{ 'cursor': 'pointer' }}>{accounts.substring(0, 15) + '...'}</span>)}
+                                        </div>
+                                        <hr />
+                                        {accounts && <div className='flex justify-center mt-4'>
+                                            <img className='me-3' src="https://assets.seracle.com/polygon-matic.svg" style={{ maxWidth: '1.5rem' }} alt="" />
+                                            <span>{balance}</span>
+                                        </div>}
+
                                     </div>
                                 </div>
                             </Card>
